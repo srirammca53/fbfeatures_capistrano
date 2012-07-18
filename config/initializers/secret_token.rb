271a276aa6a850a9b70e81cb6b0b9d3b3842668c
sriram ilapakurty <sriram.in20@gmail.com>
2012-07-18 11:10:34 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbfeatures::Application.config.secret_token = 'd6ad21c3709cd53f612e7e2ae3ac7b07a56b1b2d3fa6694b93dd45bc7f0a2a1ee3c55c8cbe6df2585d17f8fab35c29db3075b8e2448f24a816c1e86cf997d46f'
